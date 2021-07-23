ea99c9c7b4336f3a8933a9343dc3ce565538df34
Dennis von Bargen <dennis@vonbargen.se>
2021-07-23 18:33:12 +0200

    Changed capitalization for builds

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ tag=$1
 key=$2
 src=$3
 
-dotnet pack -c Release -p:PackageVersion=${tag} DvBCrud.MongoDB/DvBCrud.MongoDB.csproj
-dotnet pack -c Release -p:PackageVersion=${tag} DvBCrud.MongoDB.Api/DvBCrud.MongoDB.Api.csproj
+dotnet pack -c Release -p:PackageVersion=${tag} DvBCrud.MongoDb/DvBCrud.MongoDb.csproj
+dotnet pack -c Release -p:PackageVersion=${tag} DvBCrud.MongoDb.Api/DvBCrud.MongoDb.Api.csproj
 
-dotnet nuget push DvBCrud.MongoDB/bin/Release/DvBCrud.MongoDB.${tag}.nupkg --api-key ${key} --source ${src}
-dotnet nuget push DvBCrud.MongoDB.Api/bin/Release/DvBCrud.MongoDB.Api.${tag}.nupkg --api-key ${key} --source ${src}
+dotnet nuget push DvBCrud.MongoDb/bin/Release/DvBCrud.MongoDb.${tag}.nupkg --api-key ${key} --source ${src}
+dotnet nuget push DvBCrud.MongoDb.Api/bin/Release/DvBCrud.MongoDb.Api.${tag}.nupkg --api-key ${key} --source ${src}
