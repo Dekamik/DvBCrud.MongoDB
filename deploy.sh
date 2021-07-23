abb9ac4abbcbf288237a634b9fd85009acdc4082
Dennis von Bargen <dennis@vonbargen.se>
2021-07-23 18:29:40 +0200

    Fixed builds

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ key=$2
 src=$3
 
 dotnet pack -c Release -p:PackageVersion=${tag} DvBCrud.MongoDB/DvBCrud.MongoDB.csproj
-dotnet pack -c Release -p:PackageVersion=${tag} DvBCrud.MongoDB.API/DvBCrud.MongoDB.API.csproj
+dotnet pack -c Release -p:PackageVersion=${tag} DvBCrud.MongoDB.Api/DvBCrud.MongoDB.Api.csproj
 
 dotnet nuget push DvBCrud.MongoDB/bin/Release/DvBCrud.MongoDB.${tag}.nupkg --api-key ${key} --source ${src}
-dotnet nuget push DvBCrud.MongoDB.API/bin/Release/DvBCrud.MongoDB.API.${tag}.nupkg --api-key ${key} --source ${src}
+dotnet nuget push DvBCrud.MongoDB.Api/bin/Release/DvBCrud.MongoDB.Api.${tag}.nupkg --api-key ${key} --source ${src}
