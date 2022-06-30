e4bc0e450f8b7826fe1767a15000ee2c7a750b94
Dennis von Bargen <dennis.von-bargen@viati.se>
2022-06-30 13:35:56 +0200

    Fixed example repository

--- DvBCrud.MongoDb.Api.TestApi/WeatherForecasts/WeatherForecastRepository.cs
+++ DvBCrud.MongoDb.Api.TestApi/WeatherForecasts/WeatherForecastRepository.cs
@@ -1,4 +1,5 @@
 using DvBCrud.MongoDB.Repositories;
+using DvBCrud.MongoDB.Repositories.Wrappers;
 using DvBCrud.MongoDB.Settings;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -8,7 +9,7 @@ namespace DvBCrud.MongoDb.Api.TestApi.WeatherForecasts
 {
     public class WeatherForecastRepository : Repository<WeatherForecast>
     {
-        public WeatherForecastRepository(IMongoClient client, IOptions<MongoSettings> options) : base(client, options)
+        public WeatherForecastRepository(IMongoClient client, IOptions<MongoSettings> options, IMongoCollectionWrapperFactory factory) : base(client, options, factory)
         {
         }
     }
