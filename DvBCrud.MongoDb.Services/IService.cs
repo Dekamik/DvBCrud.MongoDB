999cf339af224210b57cbf3ee11c2a0f7bf71ff2
Dennis von Bargen <dennis.von-bargen@viati.se>
2022-07-08 12:23:58 +0200

    Added services

---
+++ DvBCrud.MongoDb.Services/IService.cs
@@ -0,0 +1,14 @@
+﻿namespace DvBCrud.MongoDb.Services;
+
+public interface IService<TApiModel>
+{
+    IEnumerable<TApiModel> GetAll();
+    TApiModel? Get(string id);
+    Task<TApiModel?> GetAsync(string id);
+    void Create(TApiModel apiModel);
+    Task CreateAsync(TApiModel apiModel);
+    void Update(string id, TApiModel apiModel);
+    Task UpdateAsync(string id, TApiModel apiModel);
+    void Delete(string id);
+    Task DeleteAsync(string id);
+}
---
+++ DvBCrud.MongoDb.Services/ISimpleService.cs
@@ -0,0 +1,10 @@
+﻿namespace DvBCrud.MongoDb.Services;
+
+/// <summary>
+/// ISimpleService is a simple default implementation to expose data models directly
+/// </summary>
+/// <typeparam name="TDataModel"></typeparam>
+public interface ISimpleService<TDataModel> : IService<TDataModel>
+{
+    
+}
